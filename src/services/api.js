e37a6d35050eaa326b3a6129a1685c5deb0de9c8
Gull Snobar <gullsnobar07@gmail.com>
2025-07-26 15:38:57 +0500

    First Commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "1d4d1875edb96fdebaca84d62437f76e";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
